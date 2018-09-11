c6a44b87a142c858b222462cb7ffd9f6e8551ea8
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2018-09-11 13:45:29 +0200

    poprawka 21 11.09.2018

--- app.js
+++ app.js
@@ -215,9 +215,9 @@ function handleEcho(messageId, appId, metadata) {
 function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
 	switch (action) {
         case "iphonexxx_colors.favourite":
-            colors.updateUserColor(parameters.fields['job-vacancy'].stringValue, sender);
-            //let reply = `OK, we need more information about you. Do you agree to start recruitment`;
-            //sendTextMessage(sender, reply);
+            //colors.updateUserColor(parameters.fields['job-vacancy'].stringValue, sender);
+            let reply = `OK, we need more information about you. Do you agree to start recruitment`;
+            sendTextMessage(sender, reply);
             break;
         case "buy.iphone":
             colors.readUserColor(function(color) {
