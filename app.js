651acf5f87010347b59877b0e2ef1975c43ff457
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2018-09-11 11:50:53 +0200

    poprawka 11 11.09.2018

--- app.js
+++ app.js
@@ -237,6 +237,25 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
                 let allColorsString = allColors.join(', ');
                 let reply = `We're currently looking for new staff (${allColorsString}). Please click or type text which job do you prefer.`;
                 sendTextMessage(sender, reply);
+
+                let replies = [
+                    {
+                        "content_type":"text",
+                        "title":"RPA developer",
+                        "payload":"RPA_DEVELOPER"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"Manager",
+                        "payload":"MANAGER"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"Not interested",
+                        "payload":"NOT_INTERESTED"
+                    }
+                ];
+                sendQuickReply(sender, messages[0].text.text[0], replies);
             });
             break;
 		/*case "faq-delivery":
