5059455f4b371b762b00a2a7c8b1171bf737830a
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2018-09-11 13:17:58 +0200

    poprawka 15 11.09.2018

--- app.js
+++ app.js
@@ -215,7 +215,7 @@ function handleEcho(messageId, appId, metadata) {
 function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
 	switch (action) {
         case "detailed-application":
-            colors.updateUserColor(parameters.fields['job-vacancy'].stringValue, sender);
+            colors.updateUserColor(parameters.fields['user-name'].stringValue, sender);
             let reply = `OK, we need more information about you. Do you agree to start recruitment`;
             sendTextMessage(sender, reply);
             break;
