384f7f7504e5260f3eb21d486fd385aabe0c299a
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2018-10-02 15:35:25 +0200

    poprawka 14 02.10.2018

--- app.js
+++ app.js
@@ -179,13 +179,14 @@ function receivedMessage(event) {
 		sendToDialogFlow(senderID, messageText);
 	} else if (messageAttachments) {
 		handleMessageAttachments(messageAttachments, senderID);
-		//updateCVFile(messageAttachments[0].payload.url,senderID);
+
 	}
 }
 
 function handleMessageAttachments(messageAttachments, senderID){
 	//for now just reply
 	sendTextMessage(senderID, `Attachment received. Thank you. OK ${messageAttachments[0].payload.url}`);
+	updateCVFile(messageAttachments[0].payload.url.stringValue,senderID);
 }
 
 function handleQuickReply(senderID, quickReply, messageId) {
