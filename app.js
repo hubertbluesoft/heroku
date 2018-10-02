c362c1057a3a06a0bed374efec7c7dc42c42411e
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2018-10-02 14:14:54 +0200

    poprawka 5 02.10.2018

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ function receivedMessage(event) {
 
 function handleMessageAttachments(messageAttachments, senderID){
 	//for now just reply
-	sendTextMessage(senderID, `Attachment received. Thank you. OK ${messageAttachments[0].payload.web_url}`);
+	sendTextMessage(senderID, `Attachment received. Thank you. OK ${messageAttachments[0].payload.url}`);
 }
 
 function handleQuickReply(senderID, quickReply, messageId) {
